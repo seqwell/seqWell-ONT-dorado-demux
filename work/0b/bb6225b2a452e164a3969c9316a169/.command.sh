97550dc0dd8565b56437331ad624a16ade510413
yanyanliubio <yanyanliujs@gmail.com>
2025-10-07 15:01:01 -0400

    clean repository

--- work/0b/bb6225b2a452e164a3969c9316a169/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-dorado demux fastq_pass     --output-dir fastq_pass_Demux     --kit-name sw     --barcode-arrangement arrangement.toml     --barcode-sequences barcodes.384.fa     --emit-fastq
